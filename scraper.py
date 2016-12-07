e18938391bdfd4a0e2ae744898561602ee2e61bc
Andrés Pereira <apereira@alabs.org>
2016-12-07 12:26:22 +0100

    Cleaning up comments/python3 full conversion/fixing bug on personal URLs

--- scraper.py
+++ scraper.py
@@ -27,10 +27,6 @@ def get_content(url):
     return root.getroottree()
 
 
-def convert_to_unicode(string):
-    return string.encode('latin-1').decode('utf-8')
-
-
 class CongressList(object):
 
     def __init__(self):
@@ -41,25 +37,18 @@ class CongressList(object):
         pg = 1
         nexturl = self.start_url
         while nexturl:
-            print('Page {}'.format(pg))
             xmlroot = get_content(nexturl)
             nexturl = self.parse_and_save(xmlroot)
             pg += 1
-        print('FIN ({} páginas)'.format(pg))
 
     def parse_and_save(self, root):
         urls = root.xpath('//div[@class="listado_1"]/ul/li/a/@href')
         names = root.xpath('//div[@class="listado_1"]/ul/li/a/text()')
-        #assert(len(urls)==25)
-        #assert(len(names)==25)
         for url, nombre in zip(urls, names):
             url = 'http://www.congreso.es' + url
-            nombre = convert_to_unicode(nombre)
             ident = url.split('idDiputado=')[1].split('&')[0]
             resp = {'id': int(ident), 'nombre': nombre, 'url': url}
             self.result.append(resp)
-            #scraperwiki.sqlite.save(['id'], {'id':int(id), 'nombre': unicode(nombre, "utf-8"), 'url':url})
-            print(nombre)
         nsiguiente = root.xpath('count(//div[@class="paginacion"][1]/ul/a)')
         if nsiguiente == 2:
             return root.xpath('//div[@class="paginacion"][1]/ul/a[2]/@href')[0]
@@ -67,7 +56,6 @@ class CongressList(object):
             if 'Siguiente' in root.xpath('//div[@class="paginacion"][1]/ul/a/text()')[0]:
                 return root.xpath('//div[@class="paginacion"][1]/ul/a/@href')[0]
             else:
-                print('No hay más URLs')
                 return None
 
 
@@ -82,10 +70,8 @@ class CongressData(object):
         for s in scrape.result:
             url = s['url']
             ident = s['id']
-            print('{} {}'.format(ident, url))
             xmlroot = get_content(url)
             self.parse_and_save(url, xmlroot, ident)
-        print('FIN')
 
     def parse_and_save(self, url, root, ident):
         datos = {}
@@ -97,50 +83,20 @@ class CongressData(object):
             url_foto = ext[0]
         else:
             url_foto = "/missing.png"
-        partido = root.xpath('//div[@id="datos_diputado"]/p[@class="nombre_grupo"]/text()')[0]
         datos['url_foto'] = 'http://www.congreso.es' + url_foto
-        datos['partido'] = convert_to_unicode(partido)
-        #gif = root.xpath('substring-after(//div[@id="datos_diputado"]/p[@class="pos_hemiciclo"]/img/@src,"_")') # '100_2310.gif'
-        #e = gif.split('100_')[1].split('.')[0] #3816
-        #if len(e) == 4:
-        #    #http://www.congreso.es/wc/htdocs/web/img/hemiciclo/hemi_100_3816.gif
-        #    datos['pos_sector'] = e[0]
-        #    datos['pos_fila'] = e[1]
-        #    datos['pos_butaca'] = e[2:4]
-        #elif len(e) == 1:
-        #    #http://www.congreso.es/wc/htdocs/web/img/hemiciclo/hemi_100_3.gif
-        #    datos['pos_sector'] = 6
-        #    datos['pos_fila'] = 0
-        #    datos['pos_butaca'] = e
-        #else:
-        #    print len(e), e
-        #    assert(False)
-        # --- div datos_diputado ---
+        datos['partido'] = root.xpath('//div[@id="datos_diputado"]/p[@class="nombre_grupo"]/text()')[0]
         datos['legislatura'] = root.xpath('//div[@id="curriculum"]/div[@class="principal"]/text()')[0].strip()
-        apellidos = root.xpath('substring-before(//div[@id="curriculum"]/div[@class="nombre_dip"]/text(),",")').strip()
-        nombre = root.xpath('substring-after(//div[@id="curriculum"]/div[@class="nombre_dip"]/text(),",")').strip()
-        datos['apellidos'] = convert_to_unicode(apellidos)
-        datos['nombre'] = convert_to_unicode(nombre)
-        # Ciprià: http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BusqForm?_piref73_1333155_73_1333154_1333154.next_page=/wc/fichaDiputado?idDiputado=329&idLegislatura=11
-        # Aixalà: http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BusqForm?_piref73_1333155_73_1333154_1333154.next_page=/wc/fichaDiputado?idDiputado=190&idLegislatura=11
-        #if 'Cipri' in datos['nombre']:
-        #    datos['nombre'] = 'Cipria'
-        #if 'Solsona Aixal' in datos['apellidos']:
-        #    datos['apellidos'] = 'Solsona Aixala'
+        datos['apellidos'] = root.xpath('substring-before(//div[@id="curriculum"]/div[@class="nombre_dip"]/text(),",")').strip()
+        datos['nombre'] = root.xpath('substring-after(//div[@id="curriculum"]/div[@class="nombre_dip"]/text(),",")').strip()
         cargo = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][1]/ul/li/div[@class="dip_rojo"][1]/text())')
         datos['cargo'] = cargo[:cargo.find(' ')]
-        datos['circunscripcion'] = convert_to_unicode(cargo[cargo.rfind(' ') + 1:][:-1])
+        datos['circunscripcion'] = cargo[cargo.rfind(' ') + 1:][:-1]
         if datos['cargo'] not in ('Diputado', 'Diputada'):
-            print(datos['cargo'])
             assert(False)
-        nacimiento = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[1]/text())')[10:-2]
-        cargos_anteriores = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[2]/text())')
-        estado_civil = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[3]/text())')
-        curriculum = " ".join(root.xpath('//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[3]/text()')[1:]).replace('\n', '')
-        datos['nacimiento'] = convert_to_unicode(nacimiento)
-        datos['cargos_anteriores'] = convert_to_unicode(cargos_anteriores)
-        datos['estado_civil'] = convert_to_unicode(estado_civil)
-        datos['curriculum'] = convert_to_unicode(curriculum)
+        datos['nacimiento'] = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[1]/text())')[10:-2]
+        datos['cargos_anteriores'] = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[2]/text())')
+        datos['estado_civil'] = root.xpath('normalize-space(//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[3]/text())')
+        datos['curriculum'] = " ".join(root.xpath('//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[3]/text()')[1:]).replace('\n', '')
         dec_txts = root.xpath('//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[@class="regact_dip"]/a/text()')
         dec_urls = root.xpath('//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li[@class="regact_dip"]/a/@href')
         assert(len(dec_urls) == len(dec_txts))
@@ -153,8 +109,6 @@ class CongressData(object):
             elif 'Bienes' in txt:
                 datos['declaracion_bienes_url'] = url
             else:
-                print(txt)
-                print(url)
                 assert(False)
         datos['email'] = None
         datos['web'] = None
@@ -164,7 +118,6 @@ class CongressData(object):
                 datos['email'] = url.split(':')[1]
             elif url.startswith('http'):
                 url = url
-                print('http:' + url)
                 if datos['web']:
                     datos['web'] += '; ' + url
                 else:
@@ -173,7 +126,6 @@ class CongressData(object):
                 if url.startswith('www.'):
                     datos['web'] = 'http://' + url
                 else:
-                    print(url)
                     assert(False)
         datos['twitter'] = None
         datos['facebook_url'] = None
@@ -181,6 +133,7 @@ class CongressData(object):
         datos['linkedin_url'] = None
         datos['youtube_url'] = None
         datos['instagram_url'] = None
+        datos['personal_url'] = None
         personal_urls = root.xpath('//div[@id="curriculum"]/div[@class="texto_dip"][2]/ul/li/div[@class="webperso_dip"]/div[@class="webperso_dip_imagen"]/a/@href')
         for url in personal_urls:
             if 'twitter.com/' in url:
@@ -196,8 +149,7 @@ class CongressData(object):
             elif 'instagram.com/' in url:
                 datos['instagram_url'] = url
             else:
-                print(url)
-                assert(False)
+                datos['personal_url'] = url
         """
         Twitter solo: http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BusqForm?_piref73_1333155_73_1333154_1333154.next_page=/wc/fichaDiputado?idDiputado=191&idLegislatura=11
         Blog: http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BusqForm?_piref73_1333155_73_1333154_1333154.next_page=/wc/fichaDiputado?idDiputado=246&idLegislatura=11
@@ -205,11 +157,8 @@ class CongressData(object):
         Flickr: http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BusqForm?_piref73_1333155_73_1333154_1333154.next_page=/wc/fichaDiputado?idDiputado=104&idLegislatura=11
         """
         datos['comisiones'] = "; ".join(root.xpath('//div[@id="curriculum"]/div[@class="listado_1"]/ul/li/a/text()'))
-        #print('{} {}'.format(datos['nombre'], datos['apellidos']))
         scraperwiki.sqlite.save(['id'], datos)
 
 
-#s = CongressData()
-#s.start()
 scrape = CongressData()
 scrape.start()
